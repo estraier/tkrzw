f846e00b615d0dee5a057442c7b5793b234794b2
Mikio Hirabayashi <hirarin@gmail.com>
2021-06-30 16:07:54 +0900

    Move the poistion of the cyclic magic data to after the opqque data.

--- tkrzw_dbm_hash.cc
+++ tkrzw_dbm_hash.cc
@@ -44,8 +44,8 @@ constexpr int32_t META_OFFSET_EFF_DATA_SIZE = 32;
 constexpr int32_t META_OFFSET_FILE_SIZE = 40;
 constexpr int32_t META_OFFSET_MOD_TIME = 48;
 constexpr int32_t META_OFFSET_DB_TYPE = 56;
-constexpr int32_t META_OFFSET_CYCLIC_MAGIC_BACK = 63;
-constexpr int32_t META_OFFSET_OPAQUE = 64;
+constexpr int32_t META_OFFSET_OPAQUE = 62;
+constexpr int32_t META_OFFSET_CYCLIC_MAGIC_BACK = 127;
 constexpr int32_t FBP_SECTION_SIZE = 1008;
 constexpr int32_t RECORD_BASE_HEADER_SIZE = 16;
 const char RECORD_BASE_MAGIC_DATA[] = "TkrzwREC\n";
@@ -1299,10 +1299,9 @@ Status HashDBMImpl::SaveMetadata(bool finish) {
   WriteFixNum(meta + META_OFFSET_FILE_SIZE, file_size_, 8);
   WriteFixNum(meta + META_OFFSET_MOD_TIME, mod_time_, 8);
   WriteFixNum(meta + META_OFFSET_DB_TYPE, db_type_, 4);
-  WriteFixNum(meta + META_OFFSET_CYCLIC_MAGIC_BACK, cyclic_magic_, 1);
-  const int32_t opaque_size =
-      std::min<int32_t>(opaque_.size(), METADATA_SIZE - META_OFFSET_OPAQUE);
+  const int32_t opaque_size = std::min<int32_t>(opaque_.size(), HashDBM::OPAQUE_METADATA_SIZE);
   std::memcpy(meta + META_OFFSET_OPAQUE, opaque_.data(), opaque_size);
+  WriteFixNum(meta + META_OFFSET_CYCLIC_MAGIC_BACK, cyclic_magic_, 1);
   return file_->Write(0, meta, METADATA_SIZE);
 }
 
@@ -2378,8 +2377,8 @@ Status HashDBM::ReadMetadata(
   *file_size = ReadFixNum(meta + META_OFFSET_FILE_SIZE, 8);
   *mod_time = ReadFixNum(meta + META_OFFSET_MOD_TIME, 8);
   *db_type = ReadFixNum(meta + META_OFFSET_DB_TYPE, 4);
+  *opaque = std::string(meta + META_OFFSET_OPAQUE, HashDBM::OPAQUE_METADATA_SIZE);
   const int32_t cyclic_magic_back = ReadFixNum(meta + META_OFFSET_CYCLIC_MAGIC_BACK, 1);
-  *opaque = std::string(meta + META_OFFSET_OPAQUE, METADATA_SIZE - META_OFFSET_OPAQUE);
   if (*cyclic_magic != cyclic_magic_back) {
     *cyclic_magic = -1;
   }
