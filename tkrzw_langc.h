465ef08050bc7323e606787b061c0a1d1f539516
Mikio Hirabayashi <hirarin@gmail.com>
2023-02-28 22:01:09 +0900

    Add comment.

--- tkrzw_langc.h
+++ tkrzw_langc.h
@@ -939,9 +939,13 @@ bool tkrzw_dbm_is_ordered(TkrzwDBM* dbm);
  * @param mode The search mode.  "contain" extracts keys containing the pattern.  "begin"
  * extracts keys beginning with the pattern.  "end" extracts keys ending with the pattern.
  * "regex" extracts keys partially matches the pattern of a regular expression.  "edit"
- * extracts keys whose edit distance to the pattern is the least.  Ordered databases support
- * "upper" and "lower" which extract keys whose positions are equal to or upper/lower than the
- * pattern.  "upperex" and "lowerex" are their exclusive versions.
+ * extracts keys whose edit distance to the pattern is the least.  "editbin" extracts keys whose
+ * edit distance to the binary pattern is the least.  "containcase", "containword", and
+ * "containcaseword" extract keys considering case and word boundary.  "contain*", "containcase*",
+ * "containword*", and "containcaseword*" take a null-code-separatable pattern and do batch
+ * operations for each element.  Ordered databases support "upper" and "lower" which extract keys
+ * whose positions are equal to or upper/lower than the pattern.  "upperex" and "lowerex" are
+ * their exclusive versions.
  * @param pattern_ptr The pattern pointer.
  * @param pattern_size The pattern size.  If it is negative, strlen(pattern_ptr) is used.
  * @param capacity The maximum records to obtain.  0 means unlimited.
